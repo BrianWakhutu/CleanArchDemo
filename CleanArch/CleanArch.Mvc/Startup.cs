a12777aa9a7d94cd58961a961e88bf11fe3abe5f
Brian Wakhutu <brian.wakhutu@gmail.com>
2019-07-23 10:30:43 +0300

    Set up the API with swagger

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.IoC;
+using MediatR;
 
 namespace CleanArch.Mvc
 {
@@ -48,6 +49,7 @@ namespace CleanArch.Mvc
                  options.UseSqlServer(
                      Configuration.GetConnectionString("UniversityDBConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMediatR(typeof(Startup));
             RegisterServices(services);
         }
 
